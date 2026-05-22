342fb1c3b2d39739f5e9107854fa6a78242b4d58
carol <carolineallen1012@gmail.com>
2026-05-21 20:29:52 -0400

    commit

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 """Package entry point for the CLI tool."""
 
-from lib.cli.main import main
+from cli.main import main
 
 
 if __name__ == "__main__":
--- cli/commands/user.py
+++ cli/commands/user.py
@@ -1,5 +1,5 @@
-from lib.cli.models.user import User
-from lib.cli.storage import Storage
+from cli.models.user import User
+from cli.storage import Storage
 
 storage = Storage()
 
